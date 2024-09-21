b062d018b3676774eefc8b4bda1312360bc937f4
Fatma Birel <fatmabrl11@gmail.com>
2024-09-21 21:23:17 +0300

    Implement Dapper technology for Category Project

---
+++ Project5_DapperProductProject/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Project5_DapperProductProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCategoryList = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.txtCategoryDescription = new System.Windows.Forms.TextBox();
+            this.btnCreateCategory = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCategoryId = new System.Windows.Forms.TextBox();
+            this.btnUpdateCategory = new System.Windows.Forms.Button();
+            this.btnDeleteCategory = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblCategoryCount = new System.Windows.Forms.Label();
+            this.lblProductCount = new System.Windows.Forms.Label();
+            this.lblSeaFoodProductTotalPrice = new System.Windows.Forms.Label();
+            this.lblAvgProductStock = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCategoryList
+            // 
+            this.btnCategoryList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCategoryList.Location = new System.Drawing.Point(404, 18);
+            this.btnCategoryList.Name = "btnCategoryList";
+            this.btnCategoryList.Size = new System.Drawing.Size(163, 35);
+            this.btnCategoryList.TabIndex = 0;
+            this.btnCategoryList.Text = "Listele";
+            this.btnCategoryList.UseVisualStyleBackColor = true;
+            this.btnCategoryList.Click += new System.EventHandler(this.btnCategoryList_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 181);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(544, 183);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCategoryName.Location = new System.Drawing.Point(196, 57);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(176, 29);
+            this.txtCategoryName.TabIndex = 2;
+            // 
+            // txtCategoryDescription
+            // 
+            this.txtCategoryDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCategoryDescription.Location = new System.Drawing.Point(196, 99);
+            this.txtCategoryDescription.Multiline = true;
+            this.txtCategoryDescription.Name = "txtCategoryDescription";
+            this.txtCategoryDescription.Size = new System.Drawing.Size(176, 76);
+            this.txtCategoryDescription.TabIndex = 3;
+            // 
+            // btnCreateCategory
+            // 
+            this.btnCreateCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCreateCategory.Location = new System.Drawing.Point(404, 55);
+            this.btnCreateCategory.Name = "btnCreateCategory";
+            this.btnCreateCategory.Size = new System.Drawing.Size(163, 35);
+            this.btnCreateCategory.TabIndex = 4;
+            this.btnCreateCategory.Text = "Ekle";
+            this.btnCreateCategory.UseVisualStyleBackColor = true;
+            this.btnCreateCategory.Click += new System.EventHandler(this.btnCreateCategory_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(88, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kategori Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(78, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kategori Adı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(19, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kategori Açıklaması:";
+            // 
+            // txtCategoryId
+            // 
+            this.txtCategoryId.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCategoryId.Location = new System.Drawing.Point(196, 18);
+            this.txtCategoryId.Name = "txtCategoryId";
+            this.txtCategoryId.Size = new System.Drawing.Size(176, 29);
+            this.txtCategoryId.TabIndex = 8;
+            // 
+            // btnUpdateCategory
+            // 
+            this.btnUpdateCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUpdateCategory.Location = new System.Drawing.Point(404, 134);
+            this.btnUpdateCategory.Name = "btnUpdateCategory";
+            this.btnUpdateCategory.Size = new System.Drawing.Size(163, 35);
+            this.btnUpdateCategory.TabIndex = 10;
+            this.btnUpdateCategory.Text = "Güncelle";
+            this.btnUpdateCategory.UseVisualStyleBackColor = true;
+            this.btnUpdateCategory.Click += new System.EventHandler(this.btnUpdateCategory_Click);
+            // 
+            // btnDeleteCategory
+            // 
+            this.btnDeleteCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDeleteCategory.Location = new System.Drawing.Point(404, 93);
+            this.btnDeleteCategory.Name = "btnDeleteCategory";
+            this.btnDeleteCategory.Size = new System.Drawing.Size(163, 35);
+            this.btnDeleteCategory.TabIndex = 9;
+            this.btnDeleteCategory.Text = "Sil";
+            this.btnDeleteCategory.UseVisualStyleBackColor = true;
+            this.btnDeleteCategory.Click += new System.EventHandler(this.btnDeleteCategory_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Controls.Add(this.lblSeaFoodProductTotalPrice);
+            this.groupBox1.Controls.Add(this.lblAvgProductStock);
+            this.groupBox1.Controls.Add(this.lblProductCount);
+            this.groupBox1.Controls.Add(this.lblCategoryCount);
+            this.groupBox1.Location = new System.Drawing.Point(590, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 340);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dapper İstatistikler";
+            // 
+            // lblCategoryCount
+            // 
+            this.lblCategoryCount.AutoSize = true;
+            this.lblCategoryCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCategoryCount.Location = new System.Drawing.Point(7, 20);
+            this.lblCategoryCount.Name = "lblCategoryCount";
+            this.lblCategoryCount.Size = new System.Drawing.Size(39, 24);
+            this.lblCategoryCount.TabIndex = 0;
+            this.lblCategoryCount.Text = "text";
+            // 
+            // lblProductCount
+            // 
+            this.lblProductCount.AutoSize = true;
+            this.lblProductCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblProductCount.Location = new System.Drawing.Point(7, 55);
+            this.lblProductCount.Name = "lblProductCount";
+            this.lblProductCount.Size = new System.Drawing.Size(39, 24);
+            this.lblProductCount.TabIndex = 1;
+            this.lblProductCount.Text = "text";
+            // 
+            // lblSeaFoodProductTotalPrice
+            // 
+            this.lblSeaFoodProductTotalPrice.AutoSize = true;
+            this.lblSeaFoodProductTotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSeaFoodProductTotalPrice.Location = new System.Drawing.Point(7, 126);
+            this.lblSeaFoodProductTotalPrice.Name = "lblSeaFoodProductTotalPrice";
+            this.lblSeaFoodProductTotalPrice.Size = new System.Drawing.Size(39, 24);
+            this.lblSeaFoodProductTotalPrice.TabIndex = 3;
+            this.lblSeaFoodProductTotalPrice.Text = "text";
+            // 
+            // lblAvgProductStock
+            // 
+            this.lblAvgProductStock.AutoSize = true;
+            this.lblAvgProductStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAvgProductStock.Location = new System.Drawing.Point(7, 91);
+            this.lblAvgProductStock.Name = "lblAvgProductStock";
+            this.lblAvgProductStock.Size = new System.Drawing.Size(39, 24);
+            this.lblAvgProductStock.TabIndex = 2;
+            this.lblAvgProductStock.Text = "text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(945, 376);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnUpdateCategory);
+            this.Controls.Add(this.btnDeleteCategory);
+            this.Controls.Add(this.txtCategoryId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreateCategory);
+            this.Controls.Add(this.txtCategoryDescription);
+            this.Controls.Add(this.txtCategoryName);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCategoryList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCategoryList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtCategoryName;
+        private System.Windows.Forms.TextBox txtCategoryDescription;
+        private System.Windows.Forms.Button btnCreateCategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCategoryId;
+        private System.Windows.Forms.Button btnUpdateCategory;
+        private System.Windows.Forms.Button btnDeleteCategory;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblCategoryCount;
+        private System.Windows.Forms.Label lblProductCount;
+        private System.Windows.Forms.Label lblSeaFoodProductTotalPrice;
+        private System.Windows.Forms.Label lblAvgProductStock;
+    }
+}
+
---
+++ Project5_DapperProductProject/Form1.cs
@@ -0,0 +1,83 @@
+﻿using Dapper;
+using Project5_DapperProductProject.Dtos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project5_DapperProductProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("Server=DESKTOP-Q270QVE\\SQLEXPRESS; initial catalog=northwind; integrated security= true");
+        private async void btnCategoryList_Click(object sender, EventArgs e)
+        {
+            string query = "Select * from Categories";
+            var values = await connection.QueryAsync<ResultCategoryDto>(query);
+            dataGridView1.DataSource = values;
+        }
+
+        private async void btnCreateCategory_Click(object sender, EventArgs e)
+        {
+            string query = "insert into categories (CategoryName,Description) Values (@p1,@p2)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@p1", txtCategoryName.Text);
+            parameters.Add("@p2", txtCategoryDescription.Text);
+            await connection.ExecuteAsync(query, parameters);
+
+        }
+
+        private async void btnDeleteCategory_Click(object sender, EventArgs e)
+        {
+            string query = "Delete From Categories Where CategoryId=@categoryId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryId",txtCategoryId.Text);
+            await connection.ExecuteAsync(query, parameters);
+        }
+
+        private async void btnUpdateCategory_Click(object sender, EventArgs e)
+        {
+            string query = "Update Categories Set CategoryName=@categoryName,Description=@categoryDescription Where CategoryId=@categoryId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryId", txtCategoryId.Text);
+            parameters.Add("@categoryName", txtCategoryName.Text);
+            parameters.Add("@categoryDescription", txtCategoryDescription.Text);
+            await connection.ExecuteAsync(query, parameters);
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            //Kategori sayısı
+            string query = "Select Count(*) from Categories";          
+            var categoryCount = await connection.ExecuteScalarAsync<int>(query);
+            lblCategoryCount.Text = "Toplam Kategori Sayısı: " + categoryCount;
+
+            //Ürün sayısı
+            string query2 = "Select Count(*) from Products";
+            var productCount = await connection.ExecuteScalarAsync<int>(query2);
+            lblProductCount.Text = "Toplam Ürün Sayısı: " + productCount;
+
+            //Ortalama ürün stok sayısı
+            string query3 = "Select Avg(UnitsInStock) from Products";
+            var avgProductStock = await connection.ExecuteScalarAsync<int>(query3);
+            lblAvgProductStock.Text = "Ortalama Ürün Stok Sayısı: " + avgProductStock;
+
+            //Deniz ürünleri toplam fiyatı 
+            string query4 = "Select Sum(UnitPrice) from Products Where CategoryId=(Select CategoryId From Categories Where CategoryName='SeaFood')";
+            var totalSeafoodPrice = await connection.ExecuteScalarAsync<decimal>(query4);
+            lblSeaFoodProductTotalPrice.Text = "Deniz Ürünleri Toplam Fiyatı: " + totalSeafoodPrice;
+        }
+
+    }
+}
